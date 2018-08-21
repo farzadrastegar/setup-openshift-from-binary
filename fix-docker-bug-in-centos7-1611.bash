65a12a82af89911e793502293d1c0921c7ec07e2
Farzad Rastegar <farzadras@gmail.com>
2018-08-21 09:07:17 -0400

    Add docker daemon reload

--- fix-docker-bug-in-centos7-1611.bash
+++ fix-docker-bug-in-centos7-1611.bash
@@ -1,2 +1,4 @@
 #!/bin/bash
 perl -i -0pe 's/ExecStart=.*?(\n\S*=)/ExecStart=\/usr\/bin\/dockerd -H unix:\/\/\/var\/run\/docker.sock\1/s' /usr/lib/systemd/system/docker.service
+systemctl daemon-reload
+systemctl restart docker.service 

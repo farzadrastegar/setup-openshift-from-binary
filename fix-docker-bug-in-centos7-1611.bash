16506250b169d5166a6ab66e47d388152e8e0a59
Farzad Rastegar <farzadras@gmail.com>
2018-08-21 09:24:01 -0400

    Add crontab

--- fix-docker-bug-in-centos7-1611.bash
+++ fix-docker-bug-in-centos7-1611.bash
@@ -1,4 +1,15 @@
 #!/bin/bash
 perl -i -0pe 's/ExecStart=.*?(\n\S*=)/ExecStart=\/usr\/bin\/dockerd -H unix:\/\/\/var\/run\/docker.sock\1/s' /usr/lib/systemd/system/docker.service
+
 systemctl daemon-reload
 systemctl restart docker.service 
+
+SCRIPT_FILE='/var/local/restart-docker.sh'
+cat << EOF > ${SCRIPT_FILE}
+#!/bin/bash
+systemctl daemon-reload
+systemctl restart docker.service
+EOF
+
+chmod 755 ${SCRIPT_FILE}
+crontab -l | { cat; echo "@reboot ${SCRIPT_FILE}"; } | crontab -
